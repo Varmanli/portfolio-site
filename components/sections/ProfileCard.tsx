0bd3479682a40718e639977414cdf024b0466c2c
varmanli <varmanliamirhosein@gmail.com>
2025-03-26 12:41:52 +0330

    dibug

--- components/sections/ProfileCard.tsx
+++ components/sections/ProfileCard.tsx
@@ -7,10 +7,10 @@ export default function ProfileCard() {
   return (
     <div className="w-fit border-6 shadow-2xl border-black p-10 bg-white mt-5 mx-3 lg:mx-0 lg:mt-15 relative">
       {/* گوشه ها */}
-      <div className="absolute w-6.5 h-6.5 bg-white border-4 border-black -top-1 -right-1"></div>
-      <div className="absolute w-6.5 h-6.5 bg-white border-4 border-black -top-1 -left-1"></div>
-      <div className="absolute w-6.5 h-6.5 bg-white border-4 border-black -bottom-1 -left-1"></div>
-      <div className="absolute w-6.5 h-6.5 bg-white border-4 border-black -bottom-1 -right-1"></div>
+      <div className="absolute w-6.5 h-6.5 bg-white border-4 border-black top-[-4%] right-[-5%]"></div>
+      <div className="absolute w-6.5 h-6.5 bg-white border-4 border-black top-[-4%] left-[-5%]"></div>
+      <div className="absolute w-6.5 h-6.5 bg-white border-4 border-black bottom-[-4%] left-[-5%]"></div>
+      <div className="absolute w-6.5 h-6.5 bg-white border-4 border-black bottom-[-4%] right-[-5%]"></div>
       {/* دایره ها */}
       <div className="absolute bg-black w-4 h-4 rounded-full top-3 left-[35%]"></div>
       <div className="absolute bg-black w-4 h-4 rounded-full top-3 right-[35%]"></div>
