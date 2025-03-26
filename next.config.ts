7d6c9b85a3de05e05109448ef921196c5188dd38
varmanli <varmanliamirhosein@gmail.com>
2025-03-26 12:01:28 +0330

    Update Next.js configuration for standalone output and font optimization; modify global styles to include font-family for Dana and correct font file paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  experimental: {
+    optimizeFonts: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
