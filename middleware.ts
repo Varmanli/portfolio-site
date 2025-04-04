e9e5b109b3cbc4b0ab0ae2c6c913ff20ee9e238a
varmanli <varmanliamirhosein@gmail.com>
2025-04-04 13:23:55 +0330

    comment midlleware

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,29 @@
-// middleware.ts
-import { NextRequest, NextResponse } from "next/server";
+// // middleware.ts
+// import { NextRequest, NextResponse } from "next/server";
 
-const protectedRoutes = ["/admin"];
+// const protectedRoutes = ["/admin"];
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("admin_token")?.value;
-  const pathname = req.nextUrl.pathname;
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get("admin_token")?.value;
+//   const pathname = req.nextUrl.pathname;
 
-  // ❌ مسیرهایی که نباید بررسی بشن
-  const publicPaths = ["/admin/login"];
-  const isPublic = publicPaths.some((path) => pathname.startsWith(path));
+//   // ❌ مسیرهایی که نباید بررسی بشن
+//   const publicPaths = ["/admin/login"];
+//   const isPublic = publicPaths.some((path) => pathname.startsWith(path));
 
-  if (isPublic) {
-    return NextResponse.next();
-  }
+//   if (isPublic) {
+//     return NextResponse.next();
+//   }
 
-  const isProtected = protectedRoutes.some((path) => pathname.startsWith(path));
+//   const isProtected = protectedRoutes.some((path) => pathname.startsWith(path));
 
-  if (isProtected && !token) {
-    return NextResponse.redirect(new URL("/admin/login", req.url));
-  }
+//   if (isProtected && !token) {
+//     return NextResponse.redirect(new URL("/admin/login", req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/admin/:path*"],
-};
+// export const config = {
+//   matcher: ["/admin/:path*"],
+// };
