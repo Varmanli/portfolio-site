6116fb68674ffb4fb66c677a58c8694080999390
varmanli <varmanliamirhosein@gmail.com>
2025-04-05 16:45:04 +0330

    fix text area

--- components/sections/IntroText.tsx
+++ components/sections/IntroText.tsx
@@ -9,10 +9,10 @@ interface PropsType {
 export default function IntroText({ title, desc }: PropsType) {
   return (
     <div className="relative flex flex-col gap-5 justify-center items-center lg:justify-end lg:items-end">
-      <textarea className="lg:rotate-[335deg] text-3xl lg:absolute right-[-17%] top-[-5%] font-bold">
+      <textarea className="resize-none border-0 outline-none bg-transparent shadow-none p-0 lg:rotate-[335deg] text-3xl lg:absolute right-[-17%] top-[-5%] font-bold">
         {title}
       </textarea>
-      <textarea className="bg-white rounded-xl border-3 mx-3 lg:mx-0 p-5 lg:w-[590px] text-lg lg:text-2xl  mt-20">
+      <textarea className="resize-none  outline-none shadow-none bg-white rounded-xl border-3 mx-3 lg:mx-0 p-5 lg:w-[590px] text-lg lg:text-2xl  mt-20">
         {desc}
       </textarea>
       <AccentButton
