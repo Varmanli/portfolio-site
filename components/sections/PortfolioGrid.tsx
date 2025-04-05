8e78478a9d83b4244b4006fee50d286524cb062e
varmanli <varmanliamirhosein@gmail.com>
2025-04-05 18:40:52 +0330

    fix ui

--- components/sections/PortfolioGrid.tsx
+++ components/sections/PortfolioGrid.tsx
@@ -5,7 +5,7 @@ export default function PortfolioGrid({ items }: { items: PortfolioItem[] }) {
   return (
     <div className="columns-1 sm:columns-2  gap-20">
       {items.map((item) => (
-        <div key={item.id} className="break-inside-avoid mb-6">
+        <div key={item.id} className="break-inside-avoid mb-20 mx-20">
           <PortfolioCard item={item} />
         </div>
       ))}
