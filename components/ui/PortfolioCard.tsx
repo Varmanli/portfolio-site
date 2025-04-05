feb8c1c0cb855957c8a4ac0667c4de155990cae2
varmanli <varmanliamirhosein@gmail.com>
2025-04-05 17:34:48 +0330

    fix error

--- components/ui/PortfolioCard.tsx
+++ components/ui/PortfolioCard.tsx
@@ -5,7 +5,7 @@ export default function PortfolioCard({ item }: { item: PortfolioItem }) {
   return (
     <div className="group relative overflow-hidden border-5 border-black bg-white shadow-xl shadow-black transition-all duration-300 hover:shadow-2xl hover:-translate-y-2 p-5">
       <Image
-        src={item.image}
+        src={item.thumbnail}
         alt={item.title}
         width={500}
         height={400}
@@ -16,9 +16,9 @@ export default function PortfolioCard({ item }: { item: PortfolioItem }) {
       <div className="flex justify-between items-start p-4">
         <div>
           <h3 className="text-xl lg:text-3xl font-bold mb-2">{item.title}</h3>
-          {item.description && (
+          {item.shortDesc && (
             <p className="text-gray-600 text-sm leading-relaxed">
-              {item.description}
+              {item.shortDesc}
             </p>
           )}
         </div>
